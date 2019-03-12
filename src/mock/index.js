ef76f77ee9e5ec9291a67eedc5b52404da377db3
gaosong <gaosong@nysochina.com>
2019-03-12 15:54:00 +0800

    新增说明

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,14 +1,14 @@
 /*
  * @Description: 引入mock.js及配置文件
  * @Author: MrGao
- * @LastEditors: Please set LastEditors
+ * @LastEditors: gaosong
  * @Date: 2019-03-12 10:35:08
- * @LastEditTime: 2019-03-12 10:54:20
+ * @LastEditTime: 2019-03-12 15:47:10
  */
 
 import Mock from 'mockjs'
 import User from './user'
 
-Mock.mock('/root/login/checkMemberLogin', 'post', () => {
-  return User.userInfo
+Mock.mock('/root/login/checkMemberLogin', 'post', () => {  // 此处会劫持/root/login/checkMemberLogin接口，并返回数据
+  return User.userInfo  // 返回模拟数据
 })
